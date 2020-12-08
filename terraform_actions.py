19f0bf4dc59d0656a2deb39b99f949bddba3aceb
prakashjha08 <49592964+prakashjha08@users.noreply.github.com>
2020-12-08 20:12:37 +0530

    Create terraform_actions.py

---
+++ terraform_actions.py
@@ -0,0 +1,16 @@
+import os
+
+WORKSPACE = os.environ.get('WORKSPACE_NAME')
+
+terraform fmt \
+          -diff=true \
+          -list=true \
+          -write=false
+
+##terraform init -force-copy -backend-config="key=prakash/terraform.tfstate" -backend-config="prakashawsbucketforterraform"
+terraform get -update
+terraform state pull
+
+terraform validate -var-file=dev.tfvars
+terraform plan --out=WORKSPACE/latestPlan.json -var-file=dev.tfvars -no-color
+aws s3 cp WORKSPACE/latestPlan.json s3://prakashawsbucketforterraform/output/WORKSPACE-tfoutput.txt
