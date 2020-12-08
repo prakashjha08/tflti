a41c173501b9dc3cc5971bc9df92d95b13bcde3a
prakashjha08 <49592964+prakashjha08@users.noreply.github.com>
2020-12-08 21:33:38 +0530

    Update terraform_actions.py

--- shared/terraform_actions.py
+++ shared/terraform_actions.py
@@ -5,11 +5,11 @@ import os
 WORKSPACE = os.environ.get('WORKSPACE_NAME')
 terraform --v
 
-terraform fmt -diff=true -list=true -write=false
+#terraform fmt -diff=true -list=true -write=false
 
 ##terraform init -force-copy -backend-config="key=prakash/terraform.tfstate" -backend-config="prakashawsbucketforterraform"
-terraform get -update
-terraform state pull
+#terraform get -update
+#terraform state pull
 
 terraform validate -var-file=dev.tfvars
 terraform plan --out=WORKSPACE/latestPlan.json -var-file=dev.tfvars -no-color
