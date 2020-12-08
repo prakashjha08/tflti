581d79f3f65a5c58dca577f37d9f68ba78e4f2ff
prakashjha08 <49592964+prakashjha08@users.noreply.github.com>
2020-12-08 21:42:28 +0530

    Update terraform_actions.py

--- shared/terraform_actions.py
+++ shared/terraform_actions.py
@@ -3,6 +3,7 @@
 import os
 
 WORKSPACE = os.environ.get('WORKSPACE_NAME')
+print(WORKSPACE)
 terraform --v
 
 #terraform fmt -diff=true -list=true -write=false
@@ -11,6 +12,6 @@ terraform --v
 #terraform get -update
 #terraform state pull
 
-terraform validate -var-file=dev.tfvars
+#terraform validate -var-file=dev.tfvars
 terraform plan --out=WORKSPACE/latestPlan.json -var-file=dev.tfvars -no-color
 aws s3 cp WORKSPACE/latestPlan.json s3://prakashawsbucketforterraform/output/WORKSPACE-tfoutput.txt
