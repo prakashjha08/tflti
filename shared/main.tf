295cd7b40a5fdd53d3e23704a9b2cad87648a321
root <prakash.jha@opstree.com>
2021-02-11 09:36:12 +0000

    Thu Feb 11 2021 - Committed via Codeherent

--- shared/main.tf
+++ shared/main.tf
@@ -5,3 +5,28 @@ module "network" {
   vpctags = var.vpctags
   subnettags = var.subnettags
 }
+
+resource "aws_codepipeline" "codepipeline" {
+  stage {
+    name = 
+    action {
+      name = 
+      provider = 
+      version = 
+      category = 
+      owner = 
+    }
+  }
+  name = 
+  role_arn = 
+  artifact_store {
+    location = 
+    type = 
+  }
+}
+
+resource "aws_ec2_tag" "ec2_tag" {
+  resource_id = 
+  key = 
+  value = 
+}
